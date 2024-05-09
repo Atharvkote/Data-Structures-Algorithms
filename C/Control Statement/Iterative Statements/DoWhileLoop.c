01214fc616a31ab68f5ebc1884699c7117c1523d
Atharva Kote <144655398+Atharvkote@users.noreply.github.com>
2024-05-09 17:10:31 +0530

    Add files via upload

---
+++ C/Control Statement/Iterative Statements/DoWhileLoop.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int i;
+    i = 1;
+      do{
+        printf("Hello World\n");
+        i++;
+        //this loop will print "Hello World " Prompt 10 times
+    }while(i !=10);
+    return 0;
+}
