c9c7924a9310a0613d273c33a680e0b20608b690
Atharva Kote <144655398+Atharvkote@users.noreply.github.com>
2024-05-09 17:03:05 +0530

    Add files via upload

---
+++ C/Control Statement/Iterative Statements/ForLoop.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+int i ;
+for(i=1 ; i<10 ; i++){
+    printf("Hello World !!");
+    //This Loop will print Hello World Prompt 10 times
+    }
+return 0 ;
+}
---
+++ C/Control Statement/Iterative Statements/WhileLoop.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int i;
+    i = 1;
+    while (i != 10)
+    {
+        printf("Hello World\n");
+        i++;
+        //this loop will print "Hello World " Prompt 10 times
+    }
+    return 0;
+}
