897d8eb077b184c103cb4b4180989d940d401e1c
Atharva Kote <atharvkote3@gmail.com>
2024-05-17 16:50:13 +0530

    Update LLReveralusingCOLLECTION.java

--- Java/Data Structure/Linked List/LLReveralusingCOLLECTION.java
+++ Java/Data Structure/Linked List/LLReveralusingCOLLECTION.java
@@ -1,6 +1,6 @@
 import java.util.Collections;
 class LL {
-public static void main(String []args){
+public static void main(String[] args){
 LinkedList<Integer> list2 = new LinkedList<>();
        list2.add(1);
        list2.add(2);
