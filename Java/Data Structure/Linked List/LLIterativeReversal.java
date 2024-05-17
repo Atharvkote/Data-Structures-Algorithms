1756bce5010d1ad6dbe6da3964ff77a4f66c8de2
Atharva Kote <atharvkote3@gmail.com>
2024-05-17 16:44:27 +0530

    Create LLIterativeReversal.java

---
+++ Java/Data Structure/Linked List/LLIterativeReversal.java
@@ -0,0 +1,15 @@
+public void reverseList() {
+       if(head == null || head.next == null) {
+           return;
+       }
+       Node prevNode = head;
+       Node currNode = head.next;
+       while(currNode != null) {
+           Node nextNode = currNode.next;
+           currNode.next = prevNode;
+           prevNode = currNode;
+           currNode = nextNode;
+       }
+       head.next = null;
+       head = prevNode;
+}
