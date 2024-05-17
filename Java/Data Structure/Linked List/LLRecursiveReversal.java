3f33a78d38ff61bebef4a3cf0297fe072bf94f8a
Atharva Kote <atharvkote3@gmail.com>
2024-05-17 16:54:22 +0530

    Update LLRecursiveReversal.java

--- Java/Data Structure/Linked List/LLRecursiveReversal.java
+++ Java/Data Structure/Linked List/LLRecursiveReversal.java
@@ -1,3 +1,4 @@
+class LL{
 public Node reverseListRecursive(Node head) {
        //empty node || last node or only one node
        if(head == null || head.next == null) {
@@ -8,3 +9,14 @@ public Node reverseListRecursive(Node head) {
        head.next = null;
        return newHead;
    }
+public static void main(String args[]) {
+       LL list = new LL();
+       list.addLast(1);
+       list.addLast(2);
+       list.addLast(3);
+       list.printList();
+       list.reverseListRecursive();
+       list.printList();
+ 
+     }
+}
