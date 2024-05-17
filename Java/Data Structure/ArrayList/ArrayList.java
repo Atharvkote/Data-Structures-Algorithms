2ef5fc9ba736b176ed250297887dca30a8dd9343
Atharva Kote <atharvkote3@gmail.com>
2024-05-17 14:38:17 +0530

    Update ArrayList.java

--- Java/Data Structure/ArrayList/ArrayList.java
+++ Java/Data Structure/ArrayList/ArrayList.java
@@ -1,88 +1,55 @@
 import java.util.ArrayList;
-
 import java.util.Collections;
 
-
 class ArrayLists {
-
    public static void main(String args[]) {
 
        ArrayList<Integer> list = new ArrayList<Integer>();
 
-     //  ArrayList<String> list2 = new ArrayList<String>();
-
-   //    ArrayList<Boolean> list3 = new ArrayList<Boolean>();
-
+     // ArrayList<String> list2 = new ArrayList<String>();
+   //  Declarations Syntax for ArrayList of String DataType
+  // ArrayList<Boolean> list3 = new ArrayList<Boolean>();  
+ //Declarations Syntax for ArrayList of Boolean DataType
      
-
-       //add elements
+       //add elements(appending)
 
        list.add(1);
-
        list.add(3);
-
        list.add(4);
-
        list.add(5);
-
        System.out.println(list);
 
-
-       //to get an element
-
+      //to get an element
        int element = list.get(0); // 0 is the index
-
        System.out.println(element);
 
-
        //add element in between
-
        list.add(1,2); // 1 is the index and 2 is the element to be added
-
        System.out.println(list);
-
 
        //set element
-
        list.set(0,0);
-
        System.out.println(list);
-
 
        //delete elements
-
        list.remove(0); // 0 is the index
-
        System.out.println(list);
 
-
        //size of list
-
        int size = list.size();
-
        System.out.println(size);
 
-
        //Loops on lists
-
        for(int i=0; i<list.size(); i++) {
-
            System.out.print(list.get(i) + " ");
-
        }
-
-       System.out.println();
-
+      System.out.println();
 
        //Sorting the list
-
        list.add(0);
-
        Collections.sort(list);
 
        System.out.println(list);
-
    }
-
 }
 
