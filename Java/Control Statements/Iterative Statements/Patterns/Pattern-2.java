937c61ebed5faebf9fdd6b6c26461379f8938986
Atharva Kote <atharvkote3@gmail.com>
2024-05-21 17:37:48 +0530

    Create Pattern-2.java

---
+++ Java/Control Statements/Iterative Statements/Patterns/Pattern-2.java
@@ -0,0 +1,15 @@
+public class Patterns {
+public static void inverted_rotated_halfPyramid(int n) {
+        for(int i=1; i<=n; i++) {
+            //spaces
+            for(int j=1; j<=n-i; j++) {
+                System.out.print(" ");
+            }
+            //stars
+            for(int j=1; j<=i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
