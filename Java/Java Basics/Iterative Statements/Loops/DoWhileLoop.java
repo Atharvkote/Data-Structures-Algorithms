92c6ab39dc7a92c2844c16f2a616d7a90b874642
Atharva Kote <atharvkote3@gmail.com>
2024-05-22 20:53:26 +0530

    Create DoWhileLoop.java

---
+++ Java/Java Basics/Iterative Statements/Loops/DoWhileLoop.java
@@ -0,0 +1,9 @@
+public class DoWhileLoop {
+    public static void main(String[] args) {
+        int i = 10;
+        do {
+            System.out.println(i);
+            i--;
+        } while (i>0);
+    }
+}
