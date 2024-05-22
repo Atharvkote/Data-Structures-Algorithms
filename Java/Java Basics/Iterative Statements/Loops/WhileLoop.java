706a3ca3357a8202003f97d4e4fd0507f197337b
Atharva Kote <atharvkote3@gmail.com>
2024-05-22 20:51:54 +0530

    Create WhileLoop.java

---
+++ Java/Java Basics/Iterative Statements/Loops/WhileLoop.java
@@ -0,0 +1,9 @@
+public class WhileLoop {
+    public static void main(String[] args) {
+        int i = 10;
+        while (i>0) {
+            System.out.println(i);
+            i--;
+        }
+    }
+}
