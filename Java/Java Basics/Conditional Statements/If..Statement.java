7df09abff5d8d3b513fd8f9e84a81ae34754c7e1
Atharva Kote <atharvkote3@gmail.com>
2024-05-22 21:01:56 +0530

    Create If..Statement.java

---
+++ Java/Java Basics/Conditional Statements/If..Statement.java
@@ -0,0 +1,17 @@
+/* 
+if Statement Syntax : ------>
+
+if (condition) {
+    //block of code
+
+*/
+
+public class JavaIf {
+    public static void main(String[] args) {
+        String name = "Mohan";
+        int Roll = 25;
+        if (name == "Mohan" && Roll == 25) {
+            System.out.println("Details of Mohan.");
+        }
+    }
+}
