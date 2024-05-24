ca72785de3352e042ff44f70c651f1b1d4088fa8
Atharva Kote <atharvkote3@gmail.com>
2024-05-25 01:15:38 +0530

    Update BubbleSort.java

--- Java/Sorting Algorithms/BubbleSort.java
+++ Java/Sorting Algorithms/BubbleSort.java
@@ -1,27 +1,61 @@
 import java.util.*;
-public class BubbleSort {
-	public static void main(String[] args) {
 
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter Array size : ");
-		int n = sc.nextInt();
-		int a[] = new int[n];
-		for (int i = 0; i < n; i++) {
-			System.out.println("Enter Number at " + i + " th Position : ");
-			a[i] = sc.nextInt();
-		}
-		int temp;
-		for (int i = 0 ; i < a.length; i++) {
-			for (int j = 0; j < a.length - i - 1; j++) {
-				if (a[j] > a[j + 1]) {
-					temp = a[j];
-					a[j] = a[j + 1];
-					a[j + 1] = temp;
-				}
-			}
-		}
-		for (int i = 0 ; i < a.length ; i++) {
-			System.out.print(" " + a[i]);
-		}
-	}
+//Problem : Bubble Sort
+
+public class BubbleSort {
+    public static void bubbleSort(int arr[]) {
+        for(int turn=0; turn<arr.length-1; turn++) {
+            for(int j=0; j<arr.length-1-turn; j++) {
+                if(arr[j] > arr[j+1]) {
+                    //swap
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void modifiedBubbleSort(int arr[]) {
+        for(int turn=0; turn<arr.length-1; turn++) {
+            boolean swapped = false;
+            for(int j=0; j<arr.length-1-turn; j++) {
+                if(arr[j] > arr[j+1]) {
+                    //swap
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                    swapped = true;
+                }
+            }
+            if(swapped == false) {
+                break;
+            }
+        }
+    }
+
+    public static void bubbleSortDescending(int arr[]) {
+        for(int turn=0; turn<arr.length-1; turn++) {
+            for(int j=0; j<arr.length-1-turn; j++) {
+                if(arr[j] < arr[j+1]) {
+                    //swap
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void printArr(int arr[]) {
+        for(int i=0; i<arr.length; i++) {
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+    public static void main(String args[]) {
+        int arr[] = {5, 4, 3, 2, 1};
+        bubbleSortDescending(arr);
+        printArr(arr);
+    }
 }
