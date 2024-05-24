2915dbd20d200b799fbdce1e70db84641117d305
Atharva Kote <atharvkote3@gmail.com>
2024-05-25 01:14:38 +0530

    Update InserationSort.java

--- Java/Sorting Algorithms/InserationSort.java
+++ Java/Sorting Algorithms/InserationSort.java
@@ -1,49 +1,47 @@
-
 import java.util.*;
 
+//Problem : Insertion Sort
 
-class Sorting {
-
-   public static void printArray(int arr[]) {
-
-       for(int i=0; i<arr.length; i++) {
-
-           System.out.print(arr[i]+" ");
-
-       }
-
-       System.out.println();
-
-   }
-
-
-   public static void main(String args[]) {
-
-       int arr[] = {7, 8, 1, 3, 2};
-
-
-       //insertion sort
-
+public class InsertionSort {
+    public static void insertionSort(int arr[]) {
        for(int i=1; i<arr.length; i++) {
-
-           int current = arr[i];
-
-           int j = i - 1;
-
-               while(j >= 0 && arr[j] > current) {
-
-                   //Keep swapping
-
-                   arr[j+1] = arr[j];
-
-                   j--;
-
-               }
-
-           arr[j+1] = current;
-
+           int curr = arr[i];
+           int prev = i-1;
+           //to find the index where curr is to be inserted
+           while(prev >= 0 && arr[prev] > curr) {
+               arr[prev+1] = arr[prev];
+               prev--;
+           }
+           arr[prev+1] = curr;
        }
+    }
 
-       printArray(arr);
+    public static void insertionSortDescending(int arr[]) {
+        for(int i=1; i<arr.length; i++) {
+            int curr = arr[i];
+            int prev = i-1;
+            //to find the index where curr is to be inserted
+            while(prev >= 0 && arr[prev] < curr) {
+                arr[prev+1] = arr[prev];
+                prev--;
+            }
+            arr[prev+1] = curr;
+        }
+     }
 
-   }
+    public static void printArr(int arr[]) {
+        for(int i=0; i<arr.length; i++) {
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+   
+    public static void main(String args[]) {
+        int arr[] = {5, 4, 1, 3, 2};
+        insertionSortDescending(arr);
+        printArr(arr);
+
+        //Inbuilt Sorting Algo
+        //Arrays.sort(arr);
+    }
+}
