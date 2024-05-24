ed831351596f705ffdea3b32bf8ad26049822f19
Atharva Kote <atharvkote3@gmail.com>
2024-05-25 01:14:07 +0530

    Update SelectionSort.java

--- Java/Sorting Algorithms/SelectionSort.java
+++ Java/Sorting Algorithms/SelectionSort.java
@@ -1,55 +1,49 @@
 import java.util.*;
 
+//Problem : Selection Sort
 
-class Sorting {
+public class SelectionSort {
+    public static void selectionSort(int arr[]) {
+        for(int turn=0; turn<arr.length; turn++) {
+            int minPos = turn;
+            for(int j=turn+1; j<arr.length; j++) {
+                if(arr[minPos] > arr[j]) {
+                    minPos = j;
+                }
+            } 
 
-   public static void printArray(int arr[]) {
+            //swap
+            int temp = arr[turn];
+            arr[turn] = arr[minPos];
+            arr[minPos] = temp;
+        }
+    }
 
-       for(int i=0; i<arr.length; i++) {
+    public static void selectionSortDescending(int arr[]) {
+        for(int turn=0; turn<arr.length; turn++) {
+            int minPos = turn;
+            for(int j=turn+1; j<arr.length; j++) {
+                if(arr[minPos] < arr[j]) {
+                    minPos = j;
+                }
+            } 
 
-           System.out.print(arr[i]+" ");
+            //swap
+            int temp = arr[turn];
+            arr[turn] = arr[minPos];
+            arr[minPos] = temp;
+        }
+    }
 
-       }
-
-       System.out.println();
-
-   }
-
-
-   public static void main(String args[]) {
-
-       int arr[] = {7, 8, 1, 3, 2};
-
-
-       //selection sort
-
-       for(int i=0; i<arr.length-1; i++) {
-
-           int smallest = i;
-
-           for(int j=i+1; j<arr.length; j++) {
-
-               if(arr[j] < arr[smallest]) {
-
-                   smallest = j;
-
-               }
-
-           }
-
-           //swap
-
-           int temp = arr[smallest];
-
-           arr[smallest] = arr[i];
-
-           arr[i] = temp;
-
-       }
-
-
-       printArray(arr);
-
-   }
-
+    public static void printArr(int arr[]) {
+        for(int i=0; i<arr.length; i++) {
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+    public static void main(String args[]) {
+        int arr[] = {5, 4, 1, 3, 2};
+        selectionSortDescending(arr);
+        printArr(arr);
+    }
 }
