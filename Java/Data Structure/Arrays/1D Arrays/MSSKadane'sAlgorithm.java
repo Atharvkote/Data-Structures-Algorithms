489e79079d389d99d7c4fe69b5f5c392132c255c
Atharva Kote <atharvkote3@gmail.com>
2024-05-26 21:23:56 +0530

    Create MSSKadane'sAlgorithm.java

---
+++ Java/Data Structure/Arrays/1D Arrays/MSSKadane'sAlgorithm.java
@@ -0,0 +1,24 @@
+//Question : Print maximum subarray sum
+
+    //Kadane's Algorithm
+    public static void maxSubarraySum3(int arr[]) {
+        int cs = 0;
+        int ms = 0;
+
+        for(int i=0; i<arr.length; i++) {
+            cs = cs + arr[i];
+            if(cs < 0) {
+                cs = 0;
+            } 
+            ms = Math.max(ms, cs);
+        }
+
+        System.out.println("max subarray sum is : " + ms);
+    }
+    public static void main(String args[]) {
+        int arr[] = {1, -2, 6, -1, 3};
+        maxSubarraySum1(arr); 
+        maxSubarraySum2(arr);
+        maxSubarraySum3(arr);
+    }
+}
