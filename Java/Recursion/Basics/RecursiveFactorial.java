7759c94ec907507109dc03c39737867833c9e3b4
Atharva Kote <atharvkote3@gmail.com>
2024-05-26 23:53:37 +0530

    Create RecursiveFactorial.java

---
+++ Java/Recursion/Basics/RecursiveFactorial.java
@@ -0,0 +1,10 @@
+public class RecursionBasics {
+    public static int fact(int n) {
+        if(n == 0) {
+            return 1;
+        }
+        int fnm1 = fact(n-1);
+        int fn = n * fact(n-1);
+        return fn;
+    }
+}
