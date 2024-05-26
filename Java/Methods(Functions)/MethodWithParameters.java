09605bcaac51db1c92a62056c15763404f6152f4
Atharva Kote <atharvkote3@gmail.com>
2024-05-26 22:05:40 +0530

    Create MethodWithParameters.java

---
+++ Java/Methods(Functions)/MethodWithParameters.java
@@ -0,0 +1,11 @@
+public class MethodWithParameters {
+    // Method that prints a personalized greeting
+    public static void printPersonalizedGreeting(String name) {
+        System.out.println("Hello, " + name + "!");
+    }
+
+    public static void main(String[] args) {
+        // Calling the method with a parameter
+        printPersonalizedGreeting("Alice");
+    }
+}
