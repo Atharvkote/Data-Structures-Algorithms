6bc8b4fe0c92f26cae920554f11269974e9f2120
Atharva Kote <atharvkote3@gmail.com>
2024-05-26 21:58:04 +0530

    Create ForEachLoop.java

---
+++ Java/Java Basics/Iterative Statements/Loops/ForEachLoop.java
@@ -0,0 +1,11 @@
+public class ForeachExample {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5};
+
+        // Using foreach loop to iterate over the array
+        for (int number : numbers) {
+            System.out.println(number);
+          // Output :[ 1 2 3 4 5 ]
+        }
+    }
+}
