8048b9c73ef18f90e1382e9470db3c310bce6c56
Atharva Kote <atharvkote3@gmail.com>
2024-05-27 18:17:37 +0530

    Create AppendCharacter.java

---
+++ Java/Data Structure/String/String Builder/AppendCharacter.java
@@ -0,0 +1,13 @@
+// Append a char
+//Append means to add something at the end.
+
+import java.util.*;
+public class Strings {
+   public static void main(String args[]) {
+     StringBuilder sb = new StringBuilder("Tony");
+     sb.append(" Stark");
+     System.out.println(sb);
+         }
+
+    }
+
