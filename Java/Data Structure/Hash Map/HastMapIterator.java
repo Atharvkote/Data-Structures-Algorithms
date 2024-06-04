d7e82e46085592d8f17ec8c73f93d42cf993cf7b
Atharva Kote <atharvkote3@gmail.com>
2024-06-04 16:33:12 +0530

    Create HastMapIterator.java

---
+++ Java/Data Structure/Hash Map/HastMapIterator.java
@@ -0,0 +1,21 @@
+import java.util.HashMap;//Iteration (1)
+
+
+       for( Map.Entry<String, Integer> e : map.entrySet()) {
+
+           System.out.println(e.getKey());
+
+           System.out.println(e.getValue());
+
+       }
+
+
+       //Iteration (2)
+
+       Set<String> keys = map.keySet();
+
+       for(String key : keys) {
+
+           System.out.println(key+ " " + map.get(key));
+
+       }
