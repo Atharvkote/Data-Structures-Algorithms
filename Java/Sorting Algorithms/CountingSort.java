affce0664b3665a9d248cb7aa5059bc1afdf2c23
Atharva Kote <atharvkote3@gmail.com>
2024-06-06 21:24:20 +0530

    Create CountingSort.java

---
+++ Java/Sorting Algorithms/CountingSort.java
@@ -0,0 +1,20 @@
+public class Sorting {
+    public static void countingSort(int[] arr) {
+        int largest = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            largest = Math.max(largest, arr[i]);
+        }
+        int count[] = new int[largest + 1];
+        for (int i = 0; i < arr.length; i++) {
+            count[arr[i]]++;
+        }
+        //sorting 
+        int j = 0;
+        for (int i = 0; i < count.length; i++) {
+            while (count[i] > 0) {
+                arr[j] = i;
+                j++;
+                count[i]--;
+            }
+        }
+    }
