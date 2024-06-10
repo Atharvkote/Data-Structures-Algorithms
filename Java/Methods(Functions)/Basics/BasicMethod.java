6087ddbb872ba9030821da611429630330a6bff4
Atharva Kote <atharvkote3@gmail.com>
2024-06-10 18:46:10 +0530

    Add files via upload

---
+++ Java/Methods(Functions)/Basics/BasicMethod.java
@@ -0,0 +1,11 @@
+public class BasicFunction {
+    // Method that prints a greeting
+    public static void printGreeting() {
+        System.out.println("Hello, World!");
+    }
+
+    public static void main(String[] args) {
+        // Calling the method
+        printGreeting();
+    }
+}
