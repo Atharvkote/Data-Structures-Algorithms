705a2cb7d63461df029ec44f87aa51bbb8f87db3
Atharva Kote <atharvkote3@gmail.com>
2024-06-12 00:59:01 +0530

    Update InOrderTraversal.java

--- Java/Data Structure/Trees/Tree Traversals/InOrderTraversal.java
+++ Java/Data Structure/Trees/Tree Traversals/InOrderTraversal.java
@@ -1,3 +1,31 @@
+public class BinaryTreesYT {
+  
+   static class Node {
+       int data;
+       Node left;
+       Node right;
+
+       Node(int data) {
+           this.data = data;
+           this.left = null;
+           this.right = null;
+       }
+   }
+
+   static class BinaryTree {
+       static int idx = -1;
+      public static Node buildTree(int nodes[]) {
+           idx++;
+           if(nodes[idx] == -1) {
+               return null;
+           }
+           Node newNode = new Node(nodes[idx]);
+           newNode.left = buildTree(nodes);
+           newNode.right = buildTree(nodes);
+           return newNode;
+        }
+   }
+  
 public static void inorder(Node root) {
 
        if(root == null) {
@@ -14,4 +42,11 @@ public static void inorder(Node root) {
 
        inorder(root.right);
 
+}
+public static void main(String args[]) {
+       int nodes[] = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
+       BinaryTree tree = new BinaryTree()   
+
+       Node root = tree.buildTree(nodes);
+    }
 }
