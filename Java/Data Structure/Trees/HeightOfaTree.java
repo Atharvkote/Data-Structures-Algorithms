1fa33675e33528c5253787f984eac0b37a0db1d0
Atharva Kote <atharvkote3@gmail.com>
2024-06-12 00:51:21 +0530

    Create HeightOfaTree.java

---
+++ Java/Data Structure/Trees/HeightOfaTree.java
@@ -0,0 +1,16 @@
+public static int height(Node root) {
+
+       if(root == null) {
+
+           return 0;
+
+       }
+
+
+       int leftHeight = height(root.left);
+
+       int rightHeight = height(root.right);
+
+       return Math.max(leftHeight, rightHeight) + 1;
+
+}
