d01e7e5e71b1e3eb880679dcc679a328c0d045b6
Atharva Kote <atharvkote3@gmail.com>
2024-06-12 00:51:53 +0530

    Create NumberNodesInaTree.java

---
+++ Java/Data Structure/Trees/NumberNodesInaTree.java
@@ -0,0 +1,17 @@
+public static int countOfNodes(Node root) {
+
+       if(root == null) {
+
+           return 0;
+
+       }
+
+
+       int leftNodes = countOfNodes(root.left);
+
+       int rightNodes = countOfNodes(root.right);
+
+       return leftNodes + rightNodes + 1;
+
+   }
+
