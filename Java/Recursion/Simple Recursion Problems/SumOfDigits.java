14faa625fbe8b1b97284aa5c61726e65bcf13035
Atharva Kote <atharvkote3@gmail.com>
2024-06-24 19:38:01 +0530

    Create SumOfDigits.java

---
+++ Java/Recursion/Simple Recursion Problems/SumOfDigits.java
@@ -0,0 +1,13 @@
+public class DigitSum {
+    public static void main(String[] args) {
+        int ans = sum(1342);
+        System.out.println(ans);
+    }
+
+    static int sum(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        return (n % 10) + sum(n / 10);
+    }
+}
