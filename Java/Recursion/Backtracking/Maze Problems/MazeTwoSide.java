8144b9e0d8cd66fb22d0c43e739d3c88139e15bd
Atharva Kote <atharvkote3@gmail.com>
2024-06-28 20:45:09 +0530

    Update MazeTwoSide.java

--- Java/Recursion/Backtracking/Maze Problems/MazeTwoSide.java
+++ Java/Recursion/Backtracking/Maze Problems/MazeTwoSide.java
@@ -6,6 +6,8 @@ public class Maze {
   // Count Number of path possible to reach a your destination.
         path("",3,3);
 // Print the path possible 'D' for down and 'R' right because we can go in two direction only.
+        System.out.println(pathRet("", 3, 3));
+ // Print or returning in form of ArrayList.
     }
 
     static int count(int r, int c) {
@@ -30,4 +32,24 @@ public class Maze {
         if (c > 1) {
             path(p + 'R', r, c-1);
         }
+        
+     static ArrayList<String> pathRet(String p, int r, int c) {
+        if (r == 1 && c == 1) {
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+
+        ArrayList<String> list = new ArrayList<>();
+
+        if (r > 1) {
+            list.addAll(pathRet(p + 'D', r-1, c));
+        }
+
+        if (c > 1) {
+            list.addAll(pathRet(p + 'R', r, c-1));
+        }
+
+        return list;
+         }
     }
