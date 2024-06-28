cf285476c04967d5f57f046a57213ebef30f5005
Atharva Kote <atharvkote3@gmail.com>
2024-06-28 21:42:25 +0530

    Create StringSubsequence.java

---
+++ Java/Recursion/Subsequences/StringSubsequence.java
@@ -0,0 +1,62 @@
+import java.util.ArrayList;
+
+public class SubSeq {
+    public static void main(String[] args) {
+
+       subseq("","abc"); //only Subsequence
+       subseqAscii("", "abc");// direct printing
+       System.out.println(subseqAsciiRet("", "abc"));// returing a list 
+         
+    }
+
+    static void subseq(String p, String up) {
+        if (up.isEmpty()) {
+            System.out.println(p);
+            return;
+        }
+        char ch = up.charAt(0);
+        subseq(p + ch, up.substring(1));
+        subseq(p, up.substring(1));
+    }
+
+    static ArrayList<String> subseqRet(String p, String up) {
+        if (up.isEmpty()) {
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+        char ch = up.charAt(0);
+        ArrayList<String> left = subseqRet(p + ch, up.substring(1));
+        ArrayList<String> right = subseqRet(p, up.substring(1));
+
+        left.addAll(right);
+        return left;
+    }
+
+    static void subseqAscii(String p, String up) {
+        if (up.isEmpty()) {
+            System.out.println(p);
+            return;
+        }
+        char ch = up.charAt(0);
+        subseqAscii(p + ch, up.substring(1));
+        subseqAscii(p, up.substring(1));
+        subseqAscii(p + (ch+0), up.substring(1));
+    }
+
+    static ArrayList<String> subseqAsciiRet(String p, String up) {
+        if (up.isEmpty()) {
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+        char ch = up.charAt(0);
+        ArrayList<String> first = subseqAsciiRet(p + ch, up.substring(1));
+        ArrayList<String> second = subseqAsciiRet(p, up.substring(1));
+        ArrayList<String> third = subseqAsciiRet(p + (ch+0), up.substring(1));
+
+        first.addAll(second);
+        first.addAll(third);
+        return first;
+    }
+}
