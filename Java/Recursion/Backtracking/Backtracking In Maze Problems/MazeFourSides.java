7c86b9e84f8b1a5d34e88ba9d97ef27fa91d17e4
Atharva Kote <atharvkote3@gmail.com>
2024-06-28 21:21:10 +0530

    Update MazeFourSides.java

--- Java/Recursion/Backtracking/Backtracking In Maze Problems/MazeFourSides.java
+++ Java/Recursion/Backtracking/Backtracking In Maze Problems/MazeFourSides.java
@@ -1,1 +1,61 @@
-gg
+import java.util.Arrays;
+
+public class AllPaths {
+    public static void main(String[] args) {
+        boolean[][] board = {
+                {true, true, true},
+                {true, true, true},
+                {true, true, true}
+        };
+        allPath("", board, 0, 0);
+    }
+
+    static void allPath(String p, boolean[][] maze, int r, int c) {
+        if (r == maze.length - 1 && c == maze[0].length - 1) {
+            System.out.println(p);
+            return;
+        }
+
+        if (!maze[r][c]) {
+            return;
+        }
+
+        // we are considering this block in my path
+        maze[r][c] = false;
+
+        if (r < maze.length - 1) {
+            allPath(p + 'D', maze, r+1, c);
+        }
+
+        if (c < maze[0].length - 1) {
+            allPath(p + 'R', maze, r, c+1);
+        }
+
+        if (r > 0) {
+            allPath(p + 'U', maze, r-1, c);
+        }
+
+        if (c > 0) {
+            allPath(p + 'L', maze, r, c-1);
+        }
+
+        // this line is where the function will be over
+        // so before the function gets removed, also remove the changes that were made by that function
+        maze[r][c] = true;
+    }
+  /*
+  Output :
+DDRR
+DDRURD
+DDRUURDD
+DRDR
+DRRD
+DRURDD
+RDDR
+RDRD
+RDLDRR
+RRDD
+RRDLDR
+RRDLLDRR
+  */
+}
