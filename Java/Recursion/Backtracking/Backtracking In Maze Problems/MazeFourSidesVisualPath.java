368b91fd06ce54a969d699766f46cea449936144
Atharva Kote <atharvkote3@gmail.com>
2024-06-28 21:24:14 +0530

    Update MazeFourSidesVisualPath.java

--- Java/Recursion/Backtracking/Backtracking In Maze Problems/MazeFourSidesVisualPath.java
+++ Java/Recursion/Backtracking/Backtracking In Maze Problems/MazeFourSidesVisualPath.java
@@ -1,1 +1,118 @@
-hhh
+import java.util.Arrays;
+
+public class AllPaths {
+    public static void main(String[] args) {
+        boolean[][] board = {
+                {true, true, true},
+                {true, true, true},
+                {true, true, true}
+        };
+        int[][] path = new int[board.length][board[0].length];
+        allPathPrint("", board, 0, 0, path, 1);
+        
+    }
+    static void allPathPrint(String p, boolean[][] maze, int r, int c, int[][] path, int step) {
+        if (r == maze.length - 1 && c == maze[0].length - 1) {
+            path[r][c] = step;
+            for(int[] arr : path) {
+                System.out.println(Arrays.toString(arr));
+            }
+            System.out.println(p);
+            System.out.println();
+            return;
+        }
+
+        if (!maze[r][c]) {
+            return;
+        }
+
+        // i am considering this block in my path
+        maze[r][c] = false;
+        path[r][c] = step;
+        if (r < maze.length - 1) {
+            allPathPrint(p + 'D', maze, r+1, c, path, step+1);
+        }
+
+        if (c < maze[0].length - 1) {
+            allPathPrint(p + 'R', maze, r, c+1, path, step+1);
+        }
+
+        if (r > 0) {
+            allPathPrint(p + 'U', maze, r-1, c, path, step+1);
+        }
+
+        if (c > 0) {
+            allPathPrint(p + 'L', maze, r, c-1, path, step+1);
+        }
+
+        // this line is where the function will be over
+        // so before the function gets removed, also remove the changes that were made by that function
+        maze[r][c] = true;
+        path[r][c] = 0;
+    }
+  /*
+  Output: 
+  
+[1, 0, 0]
+[2, 0, 0]
+[3, 4, 5]
+DDRR
+
+[1, 0, 0]
+[2, 5, 6]
+[3, 4, 7]
+DDRURD
+
+[1, 6, 7]
+[2, 5, 8]
+[3, 4, 9]
+DDRUURDD
+
+[1, 0, 0]
+[2, 3, 0]
+[0, 4, 5]
+DRDR
+
+[1, 0, 0]
+[2, 3, 4]
+[0, 0, 5]
+DRRD
+
+[1, 4, 5]
+[2, 3, 6]
+[0, 0, 7]
+DRURDD
+
+[1, 2, 0]
+[0, 3, 0]
+[0, 4, 5]
+RDDR
+
+[1, 2, 0]
+[0, 3, 4]
+[0, 0, 5]
+RDRD
+
+[1, 2, 0]
+[4, 3, 0]
+[5, 6, 7]
+RDLDRR
+
+[1, 2, 3]
+[0, 0, 4]
+[0, 0, 5]
+RRDD
+
+[1, 2, 3]
+[0, 5, 4]
+[0, 6, 7]
+RRDLDR
+
+[1, 2, 3]
+[6, 5, 4]
+[7, 8, 9]
+RRDLLDRR
+
+
+  */
+}
