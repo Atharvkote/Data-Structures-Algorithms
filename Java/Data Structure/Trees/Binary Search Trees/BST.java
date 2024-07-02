760bc6563081fa554c562711db106059abc88031
Atharva Kote <atharvkote3@gmail.com>
2024-07-02 22:07:46 +0530

    Update BST.java

--- Java/Data Structure/Trees/Binary Search Trees/BST.java
+++ Java/Data Structure/Trees/Binary Search Trees/BST.java
@@ -99,4 +99,15 @@ class BST {
     display(node.right, "Right child of " + node.value + " : ");
   }
 
+  public static void main(String[] args) {
+    // Scanner scanner = new Scanner(System.in);
+    // BinaryTree tree = new BinaryTree();
+    // tree.populate(scanner);
+    // tree.prettyDisplay();
+
+    BST tree = new BST();
+    int[] nums = { 5, 2, 7, 1, 4, 6, 9, 8, 3, 10 };
+    tree.populate(nums);
+    tree.display();
+  }
 }
