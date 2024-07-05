1082b04c58c8b2e3f0c29767a837b8318e00e63c
Atharva Kote <atharvkote3@gmail.com>
2024-07-05 14:52:02 +0530

    Create HeapMain.java

---
+++ Java/Data Structure/Heap ( PriorityQueue )/Heaps Custom Class/HeapMain.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+class Main {
+  public static void main(String[] args) throws Exception{
+    Heap<Integer> heap = new Heap<>();
+
+    heap.insert(34);
+    heap.insert(45);
+    heap.insert(22);
+    heap.insert(89);
+    heap.insert(76);
+  }
+}
