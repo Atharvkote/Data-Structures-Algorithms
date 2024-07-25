8f767bb1d883df9ba934c2ba4d43a7243f3f04e9
Atharva Kote <atharvkote3@gmail.com>
2024-07-25 22:42:00 +0530

    Create Selection_Sort.cpp

---
+++ C++/Sorting Algorithms/Selection_Sort.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+// Function to perform Selection Sort
+void selectionSort(int arr[], int n) {
+    for(int i = 0; i < n-1; i++) {
+        // Find the minimum element in unsorted array
+        int minIndex = i;
+        for(int j = i+1; j < n; j++) {
+            if(arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+        // Swap the found minimum element with the first element
+        int temp = arr[minIndex];
+        arr[minIndex] = arr[i];
+        arr[i] = temp;
+    }
+}
+
+// Function to print an array
+void printArray(int arr[], int size) {
+    for(int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    int arr[] = {64, 25, 12, 22, 11};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    cout << "Original array: ";
+    printArray(arr, n);
+    
+    selectionSort(arr, n);
+    
+    cout << "Sorted array: ";
+    printArray(arr, n);
+    
+    return 0;
+}
