2c53bbb59b8ed0659dad8a3ed5225a24fab33332
Atharva Kote <atharvkote3@gmail.com>
2024-07-28 20:12:30 +0530

    Update SearchingClass.java

--- Java/OOPs/OOP Practice Codes/SearchingClass.java
+++ Java/OOPs/OOP Practice Codes/SearchingClass.java
@@ -3,173 +3,163 @@ import java.util.List;
 import java.util.Scanner;
 
 class Array {
-    ArrayList<Integer> array;
+	ArrayList<Integer> array;
 
-    Array(int size) {
-        array = new ArrayList<>(size);
-        // Overloaded Constructor
-    }
+	Array(int size) {
+		array = new ArrayList<>(size);
+		// Overloaded Constructor
+	}
 
-    Array() {
-        array = new ArrayList<>(5);
-        // Default Constructor
-    }
+	Array() {
+		array = new ArrayList<>(5);
+		// Default Constructor
+	}
 
-    public void insert(int num) {
-        array.add(num);
-    }
+	public void insert(int num) {
+		array.add(num);
+	}
 
-    public void insert(List<Integer> nums) {
-        array.addAll(nums);
-    }
+	public void insert(List<Integer> nums) {
+		array.addAll(nums);
+	}
 
-    public int get(int index) throws Exception {
-        if (index >= array.size()) {
-            throw new Exception("Indexing Error: ArrayIndex Out of Bound");
-        }
-        return array.get(index);
-    }
+	public int get(int index) throws Exception {
+		if (index >= array.size()) {
+			throw new Exception("Indexing Error: ArrayIndex Out of Bound");
+		}
+		return array.get(index);
+	}
 
-    public void linearSearch(int target) {
-        if (!isSorted()) {
-            System.out.print("Given Array Is Not Sorted, Sorting Given Array....\n");
-            selectionSort();
-            
-        }
-        printArray();
-        
-        System.out.print("Sorted Index  : [ ");
-        for (int i = 0; i < array.size(); i++) {
-            System.out.print(i + " ,");
-        }
-        System.out.println("]");
+	public void linearSearch(int target) {
+		if (!isSorted()) {
+			System.out.print("Given Array Is Not Sorted, Sorting Given Array....\n");
+			selectionSort();
 
-        List<Integer> occurrences = new ArrayList<>();
-        for (int i = 0; i < array.size(); i++) {
-            if (target == array.get(i)) {
-                occurrences.add(i);
-            }
-        }
+		}
+		printArray();
 
-        if (occurrences.isEmpty()) {
-            System.out.println("No Match Found");
-        } else {
-            System.out.println("Occurrences of the Target: " + occurrences);
-        }
-    }
+		System.out.print("Sorted Index  : [ ");
+		for (int i = 0; i < array.size(); i++) {
+			System.out.print(i + " ,");
+		}
+		System.out.println("]");
 
-    public void binarySearch(int target) {
-        if (!isSorted()) {
-            System.out.print("Given Array Is Not Sorted, Sorting Given Array....\nSorted Array: ");
-            selectionSort();
-            printArray();
-        }
+		List<Integer> occurrences = new ArrayList<>();
+		for (int i = 0; i < array.size(); i++) {
+			if (target == array.get(i)) {
+				occurrences.add(i);
+			}
+		}
 
-        int left = 0;
-        int right = array.size() - 1;
-        List<Integer> occurrences = new ArrayList<>();
+		if (occurrences.isEmpty()) {
+			System.out.println("No Match Found");
+		} else {
+			System.out.println("Occurrences of the Target: " + occurrences);
+		}
+	}
 
-        // Binary search for the first occurrence of the target
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
+	public void binarySearch(int target) {
+		if (!isSorted()) {
+			System.out.print("Given Array Is Not Sorted, Sorting Given Array....\nSorted Array: ");
+			selectionSort();
+			printArray();
+		}
 
-            if (array.get(mid) == target) {
-                // Finding all occurrences
-                int i = mid;
-                while (i >= 0 && array.get(i) == target) {
-                    occurrences.add(i);
-                    i--;
-                }
-                i = mid + 1;
-                while (i < array.size() && array.get(i) == target) {
-                    occurrences.add(i);
-                    i++;
-                }
-                break;
-            } else if (array.get(mid) < target) {
-                left = mid + 1;
-            } else {
-                right = mid - 1;
-            }
-        }
+		int left = 0;
+		int right = array.size() - 1;
+		int foundIndex = -1;
 
-        if (occurrences.isEmpty()) {
-            System.out.println("No Match Found");
-        } else {
-            System.out.println("Occurrences of the Target: " + occurrences);
-        }
-    }
+		while (left <= right) {
+			int mid = left + (right - left) / 2;
 
-    public void search(int target, boolean isLinearSearch) {
-        if (isLinearSearch) {
-            linearSearch(target);
-        } else {
-            binarySearch(target);
-        }
-    }
+			if (array.get(mid) == target) {
+				foundIndex = mid;
+				break;
+			} else if (array.get(mid) < target) {
+				left = mid + 1;
+			} else {
+				right = mid - 1;
+			}
+		}
 
-    public int size() {
-        return array.size();
-    }
+		if (foundIndex == -1) {
+			System.out.println("No Match Found");
+		} else {
+			System.out.println("First Occurrence of the Target: " + foundIndex);
+		}
+	}
 
-    boolean isSorted() {
-        for (int i = 1; i < array.size(); i++) {
-            if (array.get(i - 1) > array.get(i)) {
-                return false;
-            }
-        }
-        return true;
-    }
 
-    void selectionSort() {
-        for (int i = 0; i < array.size() - 1; ++i) {
-            int min_idx = i;
-            for (int j = i + 1; j < array.size(); ++j) {
-                if (array.get(j) < array.get(min_idx)) {
-                    min_idx = j;
-                }
-            }
-            int temp = array.get(min_idx);
-            array.set(min_idx, array.get(i));
-            array.set(i, temp);
-        }
-    }
+	public void search(int target, boolean isLinearSearch) {
+		if (isLinearSearch) {
+			linearSearch(target);
+		} else {
+			binarySearch(target);
+		}
+	}
 
-    void printArray() {
-        
-        System.out.print("Sorted Array:   [ ");
-        for (int i = 0; i < array.size(); i++) {
-            System.out.print(array.get(i) + " ,");
-        }
-        System.out.println("]");
-    }
+	public int size() {
+		return array.size();
+	}
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+	boolean isSorted() {
+		for (int i = 1; i < array.size(); i++) {
+			if (array.get(i - 1) > array.get(i)) {
+				return false;
+			}
+		}
+		return true;
+	}
 
-        System.out.print("Enter Array Size: ");
-        int size = sc.nextInt();
+	void selectionSort() {
+		for (int i = 0; i < array.size() - 1; ++i) {
+			int min_idx = i;
+			for (int j = i + 1; j < array.size(); ++j) {
+				if (array.get(j) < array.get(min_idx)) {
+					min_idx = j;
+				}
+			}
+			int temp = array.get(min_idx);
+			array.set(min_idx, array.get(i));
+			array.set(i, temp);
+		}
+	}
 
-        Array arr = new Array(size);
+	void printArray() {
 
-        for (int i = 0; i < size; i++) {
-            System.out.print("Enter Element at [ " + i + " ]: ");
-            int num = sc.nextInt();
-            arr.insert(num);
-        }
+		System.out.print("Sorted Array:   [ ");
+		for (int i = 0; i < array.size(); i++) {
+			System.out.print(array.get(i) + " ,");
+		}
+		System.out.println("]");
+	}
 
-        System.out.print("Searching Menu :\n1. Linear Search\n\t-> Time Complexity: O(n)\n2. Binary Search\n\t-> Time Complexity: O(log n)\nSelect search method: ");
-        int choice = sc.nextInt();
-        boolean isLinearSearch = (choice == 1);
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 
-        if (isLinearSearch) {
-            System.out.print("Enter Element to Search (Linear Search): ");
-        } else {
-            System.out.print("Enter Element to Search (Binary Search): ");
-        }
+		System.out.print("Enter Array Size: ");
+		int size = sc.nextInt();
 
-        arr.search(sc.nextInt(), isLinearSearch);
+		Array arr = new Array(size);
 
-        sc.close();
-    }
-}
+		for (int i = 0; i < size; i++) {
+			System.out.print("Enter Element at [ " + i + " ]: ");
+			int num = sc.nextInt();
+			arr.insert(num);
+		}
+
+		System.out.print("Searching Menu :\n1. Linear Search\n\t• Time Complexity: O(n)\n2. Binary Search\n\t• Time Complexity: O(log n)\nSelect search method: ");
+		int choice = sc.nextInt();
+		boolean isLinearSearch = (choice == 1);
+
+		if (isLinearSearch) {
+			System.out.print("Enter Element to Search (Linear Search): ");
+		} else {
+			System.out.print("Enter Element to Search (Binary Search): ");
+		}
+
+		arr.search(sc.nextInt(), isLinearSearch);
+
+		sc.close();
+	}
+                      }
