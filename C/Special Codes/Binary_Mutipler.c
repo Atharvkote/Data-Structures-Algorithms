ec33448e0efe31ab6728c3b4753d09cc498ff031
Atharva Kote <atharvkote3@gmail.com>
2024-08-15 22:55:25 +0530

    Update Binary_Mutipler.c

--- C/Special Codes/Binary_Mutipler.c
+++ C/Special Codes/Binary_Mutipler.c
@@ -1,72 +1,71 @@
 #include <stdio.h>
 #include <math.h>
 
-int decimalToBinary(int n) {
+int decimalToBinary(int decimal) {
     int binary = 0;
-    int rem, i = 1;
+    int remainder, placeValue = 1;
 
-    while (n != 0) {
-        rem= n % 2;
-        n /= 2;
-        binary += rem * i;
-        i *= 10;
+    while (decimal != 0) {
+        remainder = decimal % 2;
+        decimal /= 2;
+        binary += remainder * placeValue;
+        placeValue *= 10;
     }
 
     return binary;
 }
 
 int binaryToDecimal(int binary) {
-    int deci = 0, i = 0, rem;
+    int decimal = 0, power = 0, remainder;
 
     while (binary != 0) {
-        rem = binary % 10;
-        deci+= rem * pow(2, i);
+        remainder = binary % 10;
+        decimal += remainder * pow(2, power);
         binary /= 10;
-        i++;
+        power++;
     }
 
-    return deci;
+    return decimal;
 }
 
-int binaryAddition(int bits_1, int bits_2) {
+int binaryAddition(int binary1, int binary2) {
     int sum = 0;
     int carry = 0;
-    int indx = 1;
+    int placeValue = 1;
 
-    while (bits_1 > 0 || bits_2 > 0 || carry > 0) {
-        int bit_A = bits_1 % 10;
-        int bit_B = bits_2 % 10;
+    while (binary1 > 0 || binary2 > 0 || carry > 0) {
+        int bit1 = binary1 % 10;
+        int bit2 = binary2 % 10;
 
-        int bit_Sum = bit_A + bit_B + carry;
-        sum += (bit_Sum % 2) * indx;
+        int bitSum = bit1 + bit2 + carry;
+        sum += (bitSum % 2) * placeValue;
 
-        carry = bit_Sum / 2;
+        carry = bitSum / 2;
 
-        bits_1 /= 10;
-        bits_2 /= 10;
-        indx *= 10;
+        binary1 /= 10;
+        binary2 /= 10;
+        placeValue *= 10;
     }
 
     return sum;
 }
 
 int binaryMultiply(int multiplicand, int multiplier) {
-    int product = 0, bit_index = 0;
+    int product = 0;
+    int bitIndex = 0;
 
     while (multiplier != 0) {
         int bit = multiplier % 10;
 
         if (bit == 1) {
-            
-            int deci_equivalent = binaryToDecimal(multiplicand);
-            deci_equivalent <<= bit_index; 
-            int bin_equivalent = decimalToBinary(deci_equivalent);
-            product = binaryAddition(product, bin_equivalent);
-            
+            int decimalEquivalent = binaryToDecimal(multiplicand);
+            decimalEquivalent <<= bitIndex;
+            int binaryEquivalent = decimalToBinary(decimalEquivalent);
+            product = binaryAddition(product, binaryEquivalent);
         }
-        
+
         multiplier /= 10;
-        bit_index++;
+        bitIndex++;
     }
 
     return product;
@@ -75,19 +74,19 @@ int binaryMultiply(int multiplicand, int multiplier) {
 int main() {
     int multiplicand, multiplier;
 
-    printf("  》 Enter Multiplicand ( In Binary ): ");
+    printf("Enter Multiplicand (in Binary): ");
     scanf("%d", &multiplicand);
 
-    printf("  》Enter Multiplier ( In Binary ): ");
+    printf("Enter Multiplier (in Binary): ");
     scanf("%d", &multiplier);
 
-    int deci_equivalent_m=binaryToDecimal(multiplicand);
-    int deci_equivalent_mx=binaryToDecimal(multiplier);
+    int decimalMultiplicand = binaryToDecimal(multiplicand);
+    int decimalMultiplier = binaryToDecimal(multiplier);
     int product = binaryMultiply(multiplicand, multiplier);
-    int deci_equivalent_product=binaryToDecimal(product); 
-    
-    
-    printf("\t● Binary Mutlipication Results....\n\t● [ %d ]( %d ) x [ %d ](%d) = [ %d ](%d)", multiplicand, deci_equivalent_m, multiplier, deci_equivalent_mx,product,deci_equivalent_product);
+    int decimalProduct = binaryToDecimal(product);
+
+    printf("Binary Multiplication Results:\n");
+    printf("%d ( %d ) x %d ( %d ) = %d ( %d )\n", multiplicand, decimalMultiplicand, multiplier, decimalMultiplier, product, decimalProduct);
 
     return 0;
 }
