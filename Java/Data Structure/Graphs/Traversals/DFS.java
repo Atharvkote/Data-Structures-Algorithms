3e689d1573f884ab0cf2b6982576de1a35692366
Atharva Kote <atharvkote3@gmail.com>
2024-08-28 00:01:28 +0530

    Update DFS.java

--- Java/Data Structure/Graphs/Traversals/DFS.java
+++ Java/Data Structure/Graphs/Traversals/DFS.java
@@ -1,1 +1,68 @@
 
+import java.util.*;
+
+public class DFS {
+    static class Edge {
+        int src;
+        int dest;
+        int wt;
+
+        public Edge(int s, int d, int w) {
+            this.src = s;
+            this.dest = d;
+            this.wt = w;
+        }
+    }
+
+    // Method to create the graph
+    static void createGraph(ArrayList<Edge> graph[]) {
+        for (int i = 0; i < graph.length; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        graph[0].add(new Edge(0, 1, 1));
+        graph[0].add(new Edge(0, 2, 1));
+        graph[1].add(new Edge(1, 0, 1));
+        graph[1].add(new Edge(1, 3, 1));
+        graph[2].add(new Edge(2, 0, 1));
+        graph[2].add(new Edge(2, 4, 1));
+        graph[3].add(new Edge(3, 1, 1));
+        graph[3].add(new Edge(3, 4, 1));
+        graph[3].add(new Edge(3, 5, 1));
+        graph[4].add(new Edge(4, 2, 1));
+        graph[4].add(new Edge(4, 3, 1));
+        graph[4].add(new Edge(4, 5, 1));
+        graph[5].add(new Edge(5, 3, 1));
+        graph[5].add(new Edge(5, 4, 1));
+        graph[5].add(new Edge(5, 6, 1));
+        graph[6].add(new Edge(6, 5, 1));  // This line should use graph[6] not graph[5]
+    }
+
+    // DFS method to traverse the graph
+    public static void dfs(ArrayList<Edge> graph[], int curr, boolean visited[]) {
+        if (visited[curr]) {
+            return;
+        }
+        
+        System.out.print(curr + " ");
+        visited[curr] = true;
+
+        // Visit all neighbors
+        for (int i = 0; i < graph[curr].size(); i++) {
+            Edge e = graph[curr].get(i);
+            dfs(graph, e.dest, visited);
+        }
+    }
+
+    public static void main(String[] args) {
+        int V = 7;
+        ArrayList<Edge> graph[] = new ArrayList[V];
+        createGraph(graph);
+
+        boolean visited[] = new boolean[V];
+
+        // Perform DFS traversal starting from vertex 0
+        System.out.println("DFS traversal starting from vertex 0:");
+        dfs(graph, 0, visited);
+    }
+  }
